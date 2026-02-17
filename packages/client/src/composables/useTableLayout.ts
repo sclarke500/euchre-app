674e8ef59a059b752c618007049eea4acfb23f7b
Steve <steve@steves-mac-mini.lan>
2026-02-16 22:16:00 -0500

    Raise user hand 30px for testing

--- packages/client/src/composables/useTableLayout.ts
+++ packages/client/src/composables/useTableLayout.ts
@@ -137,7 +137,7 @@ export function computeTableLayout(
         break
       default: // bottom (user) - position inside table, games move to bottom after dealing
         handX = tableX
-        handY = tableBottom - handInset - 15 // Inside table, raised 15px higher
+        handY = tableBottom - handInset - 30 // Inside table, raised 30px higher
     }
 
     const handPos = { x: handX, y: handY }
