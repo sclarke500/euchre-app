ff5923985b7d19b248af8dfb7a565b632467afda
Steve <steve@steves-mac-mini.lan>
2026-02-19 20:48:37 -0500

    Card skew back to 40deg to match table tilt

--- packages/client/src/components/BoardCard.vue
+++ packages/client/src/components/BoardCard.vue
@@ -188,9 +188,9 @@ defineExpose({
     transition: margin-top var(--anim-fast) ease, filter 0.7s ease;
   }
 
-  // 3D perspective skew for cards "on table" - subtle tilt to match table surface
+  // 3D perspective skew for cards "on table" - match table's 40deg tilt
   &.table-skew .card-inner {
-    transform: perspective(600px) rotateX(18deg);
+    transform: perspective(800px) rotateX(40deg);
     transform-origin: center bottom;
   }
 }
